6c1f5b65867a1fc530669427c82300543db57162
Genivaldo <cgenivaldo46@gmail.com>
2022-03-16 09:59:40 -0300

    Validando e-mail e usuario

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import Styles from './Input.module.css'
+import React from "react";
+import Styles from "./Input.module.css";
 
-
- const Input = ({label, type, name}) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={Styles.wrapper}>
-      <label htmlFor={name} className={Styles.label} >{label}</label>
-      <input id={name} name={name} className={Styles.input} type={type} />
-      <p className={Styles.error}>Error</p>
+      <label htmlFor={name} className={Styles.label}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className={Styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={Styles.error}>{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
